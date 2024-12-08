c72f6165d8c210508b8be339ebf6dc819e8b5c77
Kirill <matveev.kirill-004@yandex.ru>
2024-12-08 04:40:28 +0300

    сделано с помощью markup

--- main.py
+++ main.py
@@ -6,29 +6,33 @@ from rss_reader import get_rss_feed
 
 def check_rss_feeds():
     while True:
-        # Получаем всех пользователей и их каналы из базы данных
-        users = db.get_all_users_and_channels()
+        try:
 
-        for user_id, channels in users:
-            for channel in channels:
-                last_sent_time = db.get_last_sent_time(user_id, channel)
-                articles = []
-                # articles = get_rss_feed(channel)  # Получаем RSS-ленту для канала
+            # Получаем всех пользователей и их каналы из базы данных
+            users = db.get_all_users_and_channels()
 
-                new_articles = []
+            for user_id, channels in users:
+                for channel in channels:
+                    last_sent_time = db.get_last_sent_time(user_id, channel)
+                    articles = []
+                    # articles = get_rss_feed(channel)  # Получаем RSS-ленту для канала
 
-                for article in articles:
-                    # Проверяем, если статья новее последней рассылки
+                    new_articles = []
 
-                    published_time = datetime.strptime(article['published'], '%a, %d %b %Y %H:%M:%S %Z')
-                    if last_sent_time is None or published_time > datetime.fromisoformat(last_sent_time):
-                        new_articles.append(article)
+                    for article in articles:
+                        # Проверяем, если статья новее последней рассылки
 
-                for article in new_articles:
-                    bot.send_message(user_id, f"{article['link']}")
+                        published_time = datetime.strptime(article['published'], '%a, %d %b %Y %H:%M:%S %Z')
+                        if last_sent_time is None or published_time > datetime.fromisoformat(last_sent_time):
+                            new_articles.append(article)
 
-                if new_articles:
-                    db.update_last_sent_time(user_id, channel)
+                    for article in new_articles:
+                        bot.send_message(user_id, f"{article['link']}")
+
+                    if new_articles:
+                        db.update_last_sent_time(user_id, channel)
+        except Exception as e:
+            print(f"Ошибка при проверке RSS-ленты: {e}")
 
         time.sleep(300)  # Проверяем каждые 5 минут
 
